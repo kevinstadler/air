ba8dce05f732f40f7bacd042b0d8927631ecdbc2
Kevin Stadler <kevin.stadler@ed.ac.uk>
2019-05-05 16:41:27 +0800

    Updates

---
+++ precache-manifest.b54e4dfb5349a988f3ac70f1c63df016.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703f99d70fe6f2cd44eaf363a8a558c3",
+    "url": "/air/index.html"
+  },
+  {
+    "revision": "60ad61e93834a2f25321",
+    "url": "/air/static/css/main.0d4f2ce4.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/air/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "60ad61e93834a2f25321",
+    "url": "/air/static/js/main.639dd469.chunk.js"
+  },
+  {
+    "revision": "411a2d7623ef4a309d3d",
+    "url": "/air/static/js/runtime~main.7ae5ebcb.js"
+  }
+]);
